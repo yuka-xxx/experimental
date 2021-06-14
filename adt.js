3e3b8b69dc72adf0252d41db9221f5435a9e49d4
Yuka <85838049+yuka-xxx@users.noreply.github.com>
2021-06-14 15:39:11 +0900

    algebraic datatypes模倣案

---
+++ adt.js
@@ -0,0 +1,37 @@
+
+const construct = (t, f) => Object.assign(Object.create(t), { match: f })
+
+
+const Maybe = {}
+
+// data constructors for Maybe
+const Nothing = () => construct(Maybe, p => p.Nothing())
+const Just = x => construct(Maybe, p => p.Just(x))
+
+// catamorphism for Maybe
+const maybe = (z, f) => m => m.match({ Nothing:() =>  z, Just:(x) => f(x) })
+
+
+const List = {}
+
+// data constructors for Maybe
+const Nil = () => construct(List, p => p.Nil())
+const Cons = (x, xs) => construct(List, p => p.Cons(x, xs))
+
+// catamorphism for List
+const list = (z, f) => xs => xs.match({ Nil:() => z, Cons:(x, xs) => f(x, list(z, f)(xs)) })
+
+
+const head = (xs) => xs.match({ Nil:() => Nothing(), Cons:(y, _) => Just(y) })
+const tail = (xs) => xs.match({ Nil:() => Nothing(), Cons:(_, ys) => Just(ys) })
+const sum = list(0, (x, y) => x + y)
+
+
+const xs = Cons(1, Cons(2, Cons(3, Nil())))
+
+console.log(sum(xs))
+
+head(xs).match({
+    Nothing:() => console.log('xs is empty'),
+    Just:(x) => console.log('head(xs) = ' + x)
+})
